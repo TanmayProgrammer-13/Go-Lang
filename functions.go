a2f34cf6c44647be3d994a9f1e0e0a7503f3be25
Mr Programmer <88921795+TanmayProgrammer-13@users.noreply.github.com>
2022-05-08 01:35:22 -0700

    functions.go
    Functions In Go

---
+++ functions.go
@@ -0,0 +1,31 @@
+// Print Funtions In Go Lang
+// 1. Print()
+// 2. Println()
+// 3. Printf()
+
+// Print() Function
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World!")
+}
+
+// Println() Function
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// Printf() Function
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello World!")
+}
