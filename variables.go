a3c73a2cdffab2188290b34831eb94621e8a12b6
Mr Programmer <88921795+TanmayProgrammer-13@users.noreply.github.com>
2022-05-07 23:24:43 -0700

    variables.go
    Variables In Go Lang

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func ()  {
+	var a string = "www.mrprogrammer.in" // This is String 
+	var b = "86" //This is a Interger  
+}
+// Variable Declaretion Without Intial Value
+func ()  {
+	var a string 
+	var b bool 
+	// In the Above Code I Have Just Defined It's Type, But Not the Value Stored In the Varialbe
+}
+// Variable After Declaretion 
+// Here I Have Declared the Variable Type & the Value Stored In the Variable
+func ()  {
+	var programmer string
+	programmer = "Tanmay"
+	fmt.Println(programmer)
+}
