1ba9d7a0f6be13802598b66ed8ffe7bc76c80ace
Mr Programmer <88921795+TanmayProgrammer-13@users.noreply.github.com>
2022-05-08 01:34:31 -0700

    go constants.go
    Constants In Go

---
+++ go constants.go
@@ -0,0 +1,49 @@
+// Constants In Go Lang
+
+package main
+
+import "fmt"
+
+const CoderAge = 14
+
+func ()  {
+	fmt.Println(CoderAge)
+}
+// Constant Types
+// 1. Typed Constants
+// 2. Untyped Constants
+
+// Types Constants
+package main
+import "fmt"
+
+const Coder int = 20 
+
+func main() {
+  fmt.Println(Coder)
+}
+// Untyped Constants
+package main
+import "fmt"
+
+const Coder = 20 
+
+func main() {
+  fmt.Println(Coder)
+}
+// Multiple Constants
+
+package main
+import ("fmt")
+
+const Coder(
+  Const1 int = 20
+  Const2 = 63.1
+  Const3 = "www.mrprogrammer.in"
+)
+
+func main() {
+  fmt.Println(Const1)
+  fmt.Println(Const2)
+  fmt.Println(Const3)
+}
